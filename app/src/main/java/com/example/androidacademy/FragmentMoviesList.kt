4368cdf27080cd17f3c70c2e955da029e3567742
Dimka <d6655767@gmail.com>
2021-03-01 22:42:06 +0300

    Finished homework 3.

--- app/src/main/java/com/example/androidacademy/FragmentMoviesList.kt
+++ app/src/main/java/com/example/androidacademy/FragmentMoviesList.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 
 class FragmentMoviesList : Fragment() {
@@ -13,4 +14,14 @@ class FragmentMoviesList : Fragment() {
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ConstraintLayout>(R.id.some_movie).setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, FragmentMoviesDetails())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
 }
